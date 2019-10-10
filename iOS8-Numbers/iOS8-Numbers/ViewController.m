4aa81372bbbb1785a4a35004bfb5ba31499558bb
Paul Solt <PaulSolt@gmail.com>
2019-10-10 14:32:54 -0400

    Number literals

--- iOS8-Numbers/iOS8-Numbers/ViewController.m
+++ iOS8-Numbers/iOS8-Numbers/ViewController.m
@@ -21,14 +21,16 @@
 	//	int is not an object (id = object in Objective-C)
 	// id = pointer type (any object ... AnyObject)
 
-	NSNumber *one = [NSNumber numberWithInt:1];
-	NSNumber *two = [NSNumber numberWithInt:2];
-	NSNumber *three = [NSNumber numberWithInt:3];
+	NSNumber *one = @1; 		// [NSNumber numberWithInt:1];
+	NSNumber *two = @2; 		// [NSNumber numberWithInt:2];
+	NSNumber *three = @3;	 	// [NSNumber numberWithInt:3];
 
 	NSArray *numArray = [NSArray arrayWithObjects:one, two, three, nil];
 	printf("numArray: %s\n", [[numArray description] UTF8String]);
-	NSLog(@"numArray: %s\n", [[numArray description] UTF8String]);
+	//NSLog(@"numArray: %s\n", [[numArray description] UTF8String]);
 
+	NSArray *numArray2 = [NSArray arrayWithObjects:@3, @4, @5, nil];
+	printf("numArray2: %s\n", [[numArray2 description] UTF8String]);
 }
 
 
