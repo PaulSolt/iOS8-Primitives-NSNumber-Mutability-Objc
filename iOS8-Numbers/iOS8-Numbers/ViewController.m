6e9074d74b12a2c643b6ec74af489b326f78348e
Paul Solt <PaulSolt@gmail.com>
2019-10-10 14:52:15 -0400

    Added more code

--- iOS8-Numbers/iOS8-Numbers/ViewController.m
+++ iOS8-Numbers/iOS8-Numbers/ViewController.m
@@ -25,12 +25,61 @@
 	NSNumber *two = @2; 		// [NSNumber numberWithInt:2];
 	NSNumber *three = @3;	 	// [NSNumber numberWithInt:3];
 
-	NSArray *numArray = [NSArray arrayWithObjects:one, two, three, nil];
+//	NSArray *numArray = [NSArray arrayWithObjects:one, two, three, nil];
+	NSArray *numArray = @[one, two, three];
 	printf("numArray: %s\n", [[numArray description] UTF8String]);
 	//NSLog(@"numArray: %s\n", [[numArray description] UTF8String]);
 
-	NSArray *numArray2 = [NSArray arrayWithObjects:@3, @4, @5, nil];
+//	NSArray *numArray2 = [NSArray arrayWithObjects:@3, @4, @5, nil];
+	NSArray *numArray2 = @[@3, @4, @5];
 	printf("numArray2: %s\n", [[numArray2 description] UTF8String]);
+	
+	
+	//	[numArray addObject: @27]; // ERROR: No visible @interface for 'NSArray' declares the selector 'addObject:'
+	// selector = method = function
+
+	// Immutable (let in Swift)
+	// Mutable (var in Swift)
+
+	// const (C constant)
+	// NSMutableArray (Objective-C variable - not constant
+	// NSArray (Objective-C constant)
+
+	NSMutableArray *array = [numArray mutableCopy];
+	[array addObject:@27];
+
+	printf("array: %s\n", [[array description] UTF8String]);
+	
+	// NSDictionary -> NSMutableDictionary
+	// NSString -> NSMutableString
+	// NSArray -> NSMutableArray
+
+	NSString *name = @"Dave";
+
+	//	NSMutableString *notMutableName = @"Dave"; // @"Dave" is immutable
+	//	[notMutableName appendFormat:@"!"];			// Technically we're a immutable NSString: Crash
+	// CRASH! Attempt to mutate immutable object with appendFormat:
+	//	printf("%s\n", notMutableName.UTF8String);
+
+	// Ask for a mutable string when trying to change it
+	NSMutableString *mutableName = [@"Dave" mutableCopy];
+	[mutableName appendFormat:@"!"];
+	printf("%s\n", mutableName.UTF8String);
+	
+	// DANGERS with Mutabilty
+	
+	// We'll see this with (copy) for @property
+	
+	NSString *favoriteColor = [@"Blue" mutableCopy];
+	printf("favoriteColor: %s\n", favoriteColor.UTF8String);
+	
+//	[favoriteColor appendFormat:@".... no RED!"]; // ERROR: No visible @interface for 'NSString' declares the selector 'appendFormat:'
+	NSMutableString *fakeColor = (NSMutableString *)favoriteColor; // WARNING: Incompatible pointer types initializing 'NSMutableString *' with an expression of type 'NSString *'
+	
+	[fakeColor appendFormat:@".... no RED!"];
+	printf("fakeColor: %s\n", fakeColor.UTF8String);
+	
+
 }
 
 
